3d52937f7eb4d4da390ba4ec17ff4ea07eb254de
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-07-26 14:36:20 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mateusz Pocztowski`,
+    description: `Front End Developer portfolio by Mateusz Pocztowski`,
+    author: `@mateusz-pocztowski`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat:300, 500, 700`],
+        display: 'swap',
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
-}
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SEO from 'components/SEO/SEO';
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+  </>
+);
+
+export default IndexPage;
